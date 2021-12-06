5d06e4ce1c91a5a8cd31b83e2cbe0193c66223cc
조현중 <chj113024@gmail.com>
2021-12-06 15:20:08 +0900

    로그인 기능 구현

--- app/src/main/java/com/chj/yummyproject/LoginActivity.java
+++ app/src/main/java/com/chj/yummyproject/LoginActivity.java
@@ -6,9 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btn_create;
+    Button btn_create, btn_login;
+    EditText login_id, login_pw;
+    URLInfo urlInfo;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +32,75 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btn_create = findViewById(R.id.btn_create);
+        btn_login = findViewById(R.id.btn_login);
+        login_id = findViewById(R.id.login_id);
+        login_pw = findViewById(R.id.login_pw);
+
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
 
         btn_create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, RegisterAllergyActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_id = login_id.getText().toString();
+                String user_pw = login_pw.getText().toString();
+
+                String url = urlInfo.getUrl();
+                url += "Login";
+                StringRequest request = new StringRequest(
+                        Request.Method.GET,
+                        url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONArray memberInfo = new JSONArray(response);
+                                    boolean found = false;
+                                    for (int i=0;i<memberInfo.length();i++) {
+                                        JSONObject info = (JSONObject) memberInfo.get(i);
+                                        String id = info.getString("member_id");
+                                        String pw = info.getString("member_pw");
+                                        String name = info.getString("member_name");
+                                        int halar = info.getInt("halar");
+                                        int vegan = info.getInt("vegan");
+                                        int egg = info.getInt("egg");
+                                        int nut = info.getInt("nut");
+                                        int fish = info.getInt("fish");
+                                        int bean = info.getInt("bean");
+
+                                        if (user_id.equals(id) && user_pw.equals(pw)) {
+                                            found = true;
+                                            Intent intent = new Intent(LoginActivity.this, mainpage.class);
+                                            intent.putExtra("member_info",info.toString());
+                                            startActivity(intent);
+                                        }
+                                    }
+                                    if (found == false) {
+                                        Toast.makeText(getApplicationContext(), "Not found ID or PW", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                );
+                requestQueue.add(request);
             }
         });
     }
--- app/src/main/java/com/chj/yummyproject/mainpage.java
+++ app/src/main/java/com/chj/yummyproject/mainpage.java
@@ -1,4 +1,47 @@
 package com.chj.yummyproject;
 
-public class mainpage {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class mainpage extends AppCompatActivity {
+    Intent intent;
+    String member_info_string;
+    String member_id, member_pw, member_name;
+    int member_halar, member_vegan, member_egg, member_nut, member_fish, member_bean;
+    Button btn_rank;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage);
+
+        btn_rank = findViewById(R.id.btn_rank);
+
+        intent = getIntent();
+        member_info_string = intent.getStringExtra("member_info");
+        Log.d("info", member_info_string);
+
+        try {
+            JSONObject member_info = new JSONObject(member_info_string);
+            member_id = member_info.getString("member_id");
+            member_pw = member_info.getString("member_pw");
+            member_name = member_info.getString("member_name");
+            member_halar = member_info.getInt("halar");
+            member_vegan = member_info.getInt("vegan");
+            member_egg = member_info.getInt("egg");
+            member_nut = member_info.getInt("nut");
+            member_fish = member_info.getInt("fish");
+            member_bean = member_info.getInt("bean");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
